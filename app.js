0c6197b745ca455d94b8126e58b83a61e38503eb
Cohorte 5 <luismigonzalez38@gmail.com>
2026-01-07 16:47:46 -0500

    feat: comentarios en el codigo

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
+// La funcion conectada al boton para que funcione
 function miFuncion() {
     let text;
+    // pide los datos al usuario
     let name = prompt("Ingrese su nombre: ");
-    let age = prompt("Ingrese su Edad: ")
+    let age = prompt("Ingrese su Edad: ");
 
+    // Se asegura de que la edad sea un entero
     if ( age != parseInt(age)){
 
         console.error("El dato en edad no es un numero");
@@ -10,18 +13,26 @@ function miFuncion() {
         text = "Datos invalidos"
 
         alert("La edad ingresada no es un numero.");
+
+    //Condicional si es menor de edad
     } else if (age < 18){
 
-        text = "Hola " + name + ", Usted es menor de edad"
+        console.log("Informacion ingresada con exito");
 
+        text = "Hola " + name + ", eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!";
+
+    //Condicional si es mayor de edad
     } else if (age >= 18){
 
-        text = "Hola " + name + ", Usted ya puede buscar trabajo" 
+        console.log("Informacion ingresada con exito");
+
+        text = "Hola " + name + ", eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!";
     } else{
 
-        text = "Informacion incorrecta"
+        text = "Informacion incorrecta";
     }
 
+    //Envia el mensaje correspondiente al html
     document.getElementById("mensaje").innerHTML = text
 
 }
